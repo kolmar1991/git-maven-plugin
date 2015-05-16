20476efef485982a42f9ae89da4ab404f940003b
Marcin <kds@o2.pl>
2015-05-16 21:51:20 +0200

    Add creating diff file

--- src/main/java/edu/agh/mojo/MyMojo.java
+++ src/main/java/edu/agh/mojo/MyMojo.java
@@ -1,25 +1,55 @@
 package edu.agh.mojo;
 
+import edu.agh.git.DiffMaker;
 import edu.agh.git.GitPropertiesUtil;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.apache.maven.project.MavenProject;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
 /**
- * Goal which touches a timestamp file.
- *
- * @goal touch
+ * @goal run
  * @phase process-sources
  */
 public class MyMojo extends AbstractMojo {
 
     /**
+     * Diff file location
+     * @parameter default-value="/"
+     */
+    private String diffLocation;
+
+    /**
+     * Should generate diff file?
+     * @parameter default-value=false
+     */
+    private boolean addDiffFile;
+
+    /**
+     * Diff file name
+     * @parameter default-value="diff.patch"
+     */
+    private String diffFileName;
+
+
+    /**
+     * Project build directory
+     *
+     * @parameter expression="${project.build.directory}"
+     * @required
+     */
+    private String projectDirectory;
+
+    /**
+     * Git directory
      * @parameter default-value="${project.basedir}/.git"
      */
     private String gitDirectory;
@@ -44,6 +74,9 @@ public class MyMojo extends AbstractMojo {
             GitPropertiesUtil gitPropertiesUtil = new GitPropertiesUtil(git);
             Properties gitProperties = gitPropertiesUtil.prepareGitProperties();
             appendPropertiesToProjects(gitProperties);
+            if (addDiffFile) {
+                createDiffFile(git);
+            }
 
         } catch (Exception e) {
             e.printStackTrace(); // perfect pattern
@@ -58,5 +91,42 @@ public class MyMojo extends AbstractMojo {
             }
         }
     }
+
+    public void createDiffFile(Repository git) throws MojoExecutionException {
+        File f = createDirIfDoesntExist();
+        File diff = new File(f, diffFileName);
+
+        createAndSaveDiff(git, diff);
+    }
+
+    private File createDirIfDoesntExist() {
+        File f = new File(projectDirectory + "/classes/" + diffLocation);
+        if (!f.exists()) {
+            f.mkdirs();
+        }
+        return f;
+    }
+
+    private void createAndSaveDiff(Repository git, File diff) throws MojoExecutionException {
+        FileOutputStream w = null;
+        try {
+            w = new FileOutputStream(diff);
+            DiffMaker diffMaker = new DiffMaker(git, w);
+            diffMaker.putDiffToStream();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + diff, e);
+        } catch (GitAPIException e) {
+            throw new MojoExecutionException("Error while creating diff content ", e);
+        } finally {
+            if (w != null) {
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
+        }
+    }
+
 
 }
