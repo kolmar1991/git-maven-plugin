3e9af8689661bfb2ed2079c9acbe84a915c2bc23
Marcin <kds@o2.pl>
2015-05-16 11:42:21 +0200

    Set up project

---
+++ src/main/java/git/MyMojo.java
@@ -0,0 +1,18 @@
+package git;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal touch
+ * @phase process-sources
+ */
+public class MyMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException {
+        getLog().info("Hello  world!");
+
+
+    }
+}
